bb1aca865bc563360f07edf5c3e45ef1cc234e5b
Niclas Hammar <nicke_hammar@hotmail.com>
2017-10-10 21:35:41 +0200

    added functionallity for showing the patients

--- src/GUI/DoctorViewController.java
+++ src/GUI/DoctorViewController.java
@@ -6,44 +6,80 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class DoctorViewController {
 	
 	@FXML
-	private TableView tv;
+	private TableView<Patient> tv;
+	@FXML
+	private TableColumn<Patient, String> nameCol;
+	@FXML
+	private TableColumn<Patient, String> lnameCol;
 	
 	@FXML
 	public void initialize() {
-		getPatients();
+		 nameCol.setCellValueFactory(new PropertyValueFactory<Patient, String>("firstName"));
+	     lnameCol.setCellValueFactory(new PropertyValueFactory<Patient, String>("lastName"));
+	     tv.getItems().setAll(getPatients());
+	     
 		
 	}
-	public void getPatients(){
+	private ObservableList<Patient> getPatients(){
 		Connection con;
+		 ObservableList<Patient> data = FXCollections.observableArrayList();
 		try {
 			con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/projecthospita?autoReconnect=true&useSSL=false", "root", "backstab1870");
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery("select fname, lname FROM projecthospita.patient;");
 			
-			ArrayList<String> fname = new <String>ArrayList();
-			ArrayList lname = new ArrayList();
 			while(rs.next()){
-				
-				fname.add(rs.getString(1));
-				lname.add(rs.getString(2));
-				
-				
-			}
-			tv.getColumns().add(1, fname);
-			tv.getColumns().add(2, lname);
-		
+				data.add(new Patient(rs.getString(1), rs.getString(2)));
+				  
+                }
+  
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return data;
 	
 	}
+	
+    private class Patient {
+    	 
+        private final SimpleStringProperty firstName;
+        private final SimpleStringProperty lastName;
+ 
+        private Patient(String fName, String lName) {
+            this.firstName = new SimpleStringProperty(fName);
+            this.lastName = new SimpleStringProperty(lName);
+        }
+ 
+        public String getFirstName() {
+            return firstName.get();
+        }
+ 
+        public void setFirstName(String fName) {
+            firstName.set(fName);
+        }
+ 
+        public String getLastName() {
+            return lastName.get();
+        }
+ 
+        public void setLastName(String fName) {
+            lastName.set(fName);
+        }
+    }
 
 }
