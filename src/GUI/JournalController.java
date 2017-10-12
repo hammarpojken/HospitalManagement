0fd98cdf3f9a8bff7a3da2e5134c9d8582b1e6bb
PontusSven <pontussvensson92@gmail.com>
2017-10-12 13:21:41 +0200

    textfields

--- src/GUI/JournalController.java
+++ src/GUI/JournalController.java
@@ -4,23 +4,30 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import GUI.DoctorViewController.Patient;
+
 public class JournalController {
 	
 	
-	Connection con =
-
-	
 	public void getJournal() {
-		
-		
-		
 	
-	
-	
-	
+		try {
+			
+			con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/projecthospita?autoReconnect=true&useSSL=false", "root", "backstab1870");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select fname, lname, adress, phone FROM projecthospita.patient;");
+				
+	    while(rs.next()) { 
+	   
+	    }
+	        fnametext.setText(rs.getString("firstname"));
+	        lnametext.setText(rs.getString("lastname"));
+	        adresstext.setText(rs.getString("adress"));
+	        phonetext.setText(rs.getString("phone"));
 		
+	    				} 
+			
+		}
 	}
-	
-	
-
+		
 }
