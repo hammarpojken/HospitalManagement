75b0b59e0ebf6d83b7b6224125f70cac0794c7df
Niclas Hammar <nicke_hammar@hotmail.com>
2017-11-07 14:42:54 +0100

    Implemented prescription, test and resultcard

--- src/Hospital/Prescription.java
+++ src/Hospital/Prescription.java
@@ -1,5 +1,138 @@
 package Hospital;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Prescription {
+	
+	private final SimpleIntegerProperty testId;
+    private final SimpleLongProperty patientId;
+    private final SimpleLongProperty doctorId;
+    private final SimpleIntegerProperty medicineId;
+    private final SimpleStringProperty prescriptionInfo;
+    private final SimpleStringProperty withdrawlAmount;
+    
+    
+    public Prescription(int test_id, Long patient_id, Long doctor_id, int medicine_id, String prescription_info, String withdrawl_amount) {
+    	this.testId = new SimpleIntegerProperty(test_id);
+    	this.patientId = new SimpleLongProperty(patient_id);
+    	this.doctorId = new SimpleLongProperty(doctor_id);
+    	this.medicineId = new SimpleIntegerProperty(medicine_id);
+    	this.prescriptionInfo = new SimpleStringProperty(prescription_info);
+    	this.withdrawlAmount = new SimpleStringProperty(withdrawl_amount);
+    	
+    }
+
+
+	public final SimpleIntegerProperty testIdProperty() {
+		return this.testId;
+	}
+	
+
+
+	public final int getTestId() {
+		return this.testIdProperty().get();
+		
+		
+	}
+	
+
+
+	public final void setTestId(final int testId) {
+		this.testIdProperty().set(testId);
+	}
+	
+
+
+	public final SimpleLongProperty patientIdProperty() {
+		return this.patientId;
+	}
+	
+
+
+	public final long getPatientId() {
+		return this.patientIdProperty().get();
+	}
+	
+
+
+	public final void setPatientId(final long patientId) {
+		this.patientIdProperty().set(patientId);
+	}
+	
+
+
+	public final SimpleLongProperty doctorIdProperty() {
+		return this.doctorId;
+	}
+	
+
+
+	public final long getDoctorId() {
+		return this.doctorIdProperty().get();
+	}
+	
+
+
+	public final void setDoctorId(final long doctorId) {
+		this.doctorIdProperty().set(doctorId);
+	}
+	
+
+
+	public final SimpleIntegerProperty medicineIdProperty() {
+		return this.medicineId;
+	}
+	
+
+
+	public final int getMedicineId() {
+		return this.medicineIdProperty().get();
+	}
+	
+
+
+	public final void setMedicineId(final int medicineId) {
+		this.medicineIdProperty().set(medicineId);
+	}
+	
+
+
+	public final SimpleStringProperty prescriptionInfoProperty() {
+		return this.prescriptionInfo;
+	}
+	
+
+
+	public final String getPrescriptionInfo() {
+		return this.prescriptionInfoProperty().get();
+	}
+	
+
+
+	public final void setPrescriptionInfo(final String prescriptionInfo) {
+		this.prescriptionInfoProperty().set(prescriptionInfo);
+	}
+	
+
+
+	public final SimpleStringProperty withdrawlAmountProperty() {
+		return this.withdrawlAmount;
+	}
+	
+
+
+	public final String getWithdrawlAmount() {
+		return this.withdrawlAmountProperty().get();
+	}
+	
+
+
+	public final void setWithdrawlAmount(final String withdrawlAmount) {
+		this.withdrawlAmountProperty().set(withdrawlAmount);
+	}
+	
+
 
 }
--- src/Hospital/ResultCard.java
+++ src/Hospital/ResultCard.java
@@ -1,18 +1,24 @@
 package Hospital;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class ResultCard {
-	private String result;
 	
-	public ResultCard(String res) {
-		this.result = res;
-	}
-
-	public String getResult() {
-		return result;
-	}
-
-	public void setResult(String result) {
-		this.result = result;
+	private final SimpleIntegerProperty resultcardId;
+    private final SimpleLongProperty patientId;
+    private final SimpleStringProperty diagnose;
+    private final SimpleStringProperty remark;
+    
+    
+	public ResultCard(int resultcard_id, long patient_id, String diagnose,String remark) {
+	
+		
+		this.resultcardId = new SimpleIntegerProperty(resultcard_id);
+		this.patientId = new SimpleLongProperty(patient_id);
+		this.diagnose = new SimpleStringProperty(diagnose);
+		this.remark = new SimpleStringProperty(remark);
 	}
 	
 
