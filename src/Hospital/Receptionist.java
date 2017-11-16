98ad6a1b5bc6e1e8d33d8f677e08d8b2573236a6
pontussven <pontussvensson92@gmail.com>
2017-11-16 10:45:33 +0100

    receptionist created

---
+++ src/Hospital/Receptionist.java
@@ -0,0 +1,11 @@
+package Hospital;
+
+import java.sql.Connection;
+
+public class Receptionist extends Person {
+	
+	 public Receptionist (String firstName, String lastName, String role) {
+		 super(firstName, lastName, role);
+		 
+	 }
+}
