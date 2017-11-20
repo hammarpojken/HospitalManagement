798703c95bc15c866d5f4c584094fc88d4d3155c
hammarpojken <nicke_hammar@hotmail.com>
2017-11-20 12:51:12 +0100

    added prescription popup

--- src/GUI/PrescriptionPopup.java
+++ src/GUI/PrescriptionPopup.java
@@ -3,12 +3,15 @@ package GUI;
 import Hospital.Prescription;
 import Utils.dbhandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class PrescriptionPopup {
 	private Prescription p;
+	private Stage stage;
 	
 	@FXML
 	private Label mName;
@@ -22,10 +25,14 @@ public class PrescriptionPopup {
 	private Label docName;
 	@FXML
 	private TextArea info;
+	@FXML
+	private Button close;
 	
 	
 	@FXML
 	public void initialize() {
+		
+		
 		
 		
 	}
@@ -36,9 +43,17 @@ public class PrescriptionPopup {
 		mName.setText(p.getMedicine().getName());
 		mType.setText(p.getMedicine().getType());
 		mVolume.setText(p.getMedicine().getVolume());
-		mWithdrawl.setText(p.getMedicine().getVolume());
+		mWithdrawl.setText(p.getWithdrawlAmount() + "");
 		docName.setText(dbhandler.getDoctor(p.getDoctorId()));
 		info.setText(p.getPrescriptionInfo());
+	}
+	@FXML
+	public void closeEvent() {
+		this.stage.close();
+		
+	}
+	public void setStage(Stage stage) {
+		this.stage = stage;
 	}
 
 }
